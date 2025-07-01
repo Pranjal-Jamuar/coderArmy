08f689e63e731e3c40457b5dd3af2c6eaf78de9b
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-01 10:36:13 +0530

    adeed a pattern

--- patterns/patterns1.cpp
+++ patterns/patterns1.cpp
@@ -7,5 +7,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" <<endl;
+    int rows;
+    cout << "Enter the number of rows:";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= rows; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
