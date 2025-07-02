2807161e5c0f0026b28aa37e548358468794c405
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-02 09:46:24 +0530

    modified pattern14

--- patterns/pattern14.cpp
+++ patterns/pattern14.cpp
@@ -11,9 +11,9 @@ void pattern14()
     cout << "Enter number of rows : ";
     cin>>rows;
 
-    for (int i = 0; i < rows; i++)
+    for (int i = rows; i > 0; i--)
     {
-        for (int j = 0; j < rows - i; j++)
+        for (int j = 0; j < i; j++)
         {
             cout << j + 1 << " ";
         }
