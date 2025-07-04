cf5313a54328f9a95268cc82e182fea541b8d224
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-04 10:16:27 +0530

    added new pattern, pattern21(Pyramid pattern)

--- patterns/pattern21.cpp
+++ patterns/pattern21.cpp
@@ -28,4 +28,18 @@ void pattern21()
         }
         cout << endl;
     }
+
+    // with 3 for loops (basic)
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n - i; j++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int k = 1; k <= 2 * i - 1; k++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
 }
