0d53fa4bda4f45373a6cce2ef7480051479a307a
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-05 13:32:48 +0530

    added index = 0 logic

--- patterns/pattern26.cpp
+++ patterns/pattern26.cpp
@@ -11,6 +11,8 @@ void pattern26()
     cout << "Enter the number of rows: ";
     cin >> n;
 
+    // with index starting from 1;
+
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n - i; j++)
@@ -24,7 +26,7 @@ void pattern26()
         cout << endl;
     }
 
-    for (int i = n; i >= 1; i--)
+    for (int i = n - 1; i >= 1; i--)
     {
         for (int j = 1; j <= n - i; j++)
         {
@@ -36,4 +38,32 @@ void pattern26()
         }
         cout << endl;
     }
+
+    // with index starting from 0;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - i - 1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = 0; k < i + 1; k++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    //
+    // for (int i = n - 2; i >= 0; i--)
+    // {
+    //     for (int j = 0; j < n - i - 1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = 0; k < i + 1; k++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
 }
