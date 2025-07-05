e9f4b18b330305761774d507d0f96b018f89625e
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-05 13:48:14 +0530

    added a new pattern, inverted pyramid with spaces in between

--- patterns/pattern27.cpp
+++ patterns/pattern27.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 void pattern27()
 {
-    int n;
+    int rows;
     cout << "Enter the number of rows: ";
-    cin >> n;
+    cin >> rows;
 
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = rows - 1; i >= 0; i--)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < rows - i - 1; j++)
         {
             cout << " ";
         }
