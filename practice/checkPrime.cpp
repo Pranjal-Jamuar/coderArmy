db1989ae3c19ab0749053c73b7fbdeaac80d8e3a
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-08 13:47:03 +0530

    refactor: optimise code to check prime, practice

--- practice/checkPrime.cpp
+++ practice/checkPrime.cpp
@@ -3,6 +3,7 @@
 //
 
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 void checkPrime()
@@ -11,16 +12,40 @@ void checkPrime()
     cout << "Enter a number: ";
     cin >> n;
 
+    // Version 1
+
+    // bool isPrime = true;
+    //
+    // if (n <= 1) {
+    //     isPrime = false;
+    // } else {
+    //     for (int i = 2; i <= n / 2; i++) {
+    //         if (n % i == 0) {
+    //             isPrime = false;
+    //             break;
+    //         }
+    //     }
+    // }
+    //
+    // if (isPrime)
+    //     cout << n << " is prime.\n";
+    // else
+    //     cout << n << " is not prime.\n";
+
+    if (n <= 1)
+    {
+        cout << n << " is not prime.\n";
+        return;
+    }
+
     bool isPrime = true;
 
-    if (n <= 1) {
-        isPrime = false;
-    } else {
-        for (int i = 2; i <= n / 2; i++) {
-            if (n % i == 0) {
-                isPrime = false;
-                break;
-            }
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            isPrime = false;
+            break;
         }
     }
 
